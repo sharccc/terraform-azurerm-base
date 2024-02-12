98d217287b7d526f8c645042fc78bce30453f4e0
Sharon Cole <sharon.cole@hashicorp.com>
2024-02-12 15:11:56 +1100

    base resource group

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    azurerm = {
+      version = ">= 3.0.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "sample_name" {
+  description = "unique name as resource identifier suffixed with resource type"
+  default     = "sharccc"
+}
+
+#TODO find the link to the regions with region ids
+variable "location" {
+  description = "location for the resource group https://learn.microsoft.com/en-us/rest/api/resources/subscriptions/list-locations"
+  default     = "Australia SouthEast"
+}
+
+variable "tag_owner" {
+  description = "email address of the owner of the resources to include as tagging"
+  default     = "https://github.com/sharccc"
+}
+
+locals {
+  tags = {
+    owner = var.tag_owner
+    base  = "https://github.com/sharccc/terraform-z-base"
+  }
+}
+
+resource "azurerm_resource_group" "sample" {
+  name     = join("-", [var.sample_name, "rg"])
+  location = var.location
+  tags     = merge(local.tags)
+}
